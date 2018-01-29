33574c0932d2d2ea196647bffc062ab35c87cc8a
Rashid Lasker <rashidlasker@gmail.com>
2018-01-28 21:55:53 -0500

    Windows finish

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# working-directory-manager
+# Working Directory Manager
+
+[![npm](https://img.shields.io/npm/l/express.svg)]()
+
 Working directory manager for switching between multiple projects
+
+## Installation
+
+Install `working-directory-manager` globally with `npm install working-directory-manager -g`, so you can use the command in any context.
+
+
+## Syntax:
+
+Note: arguments in <> are required, arguments in [] are optional
+
+`wdm <shortcut>`
+
+Searches through saved directories and opens the workspace in a new command line window.
+
+`wdm save|s <shortcut> [loc]`
+
+Saves the current directory under the alias \<shortcut>. If [loc] is given, it is saved instead.
+
+`wdm list|l [shortcut]`
+
+List all saved directories. If [shortcut] is given, the program will search for that particular alias.
+
+`wdm remove|r <shortcut>`
+
+Remove the selected directory from the list of saved aliases, if it exists.
+
+`wdm --version|-v`
+
+Print current version
+
+
+## Todo
+
+* Change command prompt directory without opening a new window/have an option to do so
+* Input sanitation and validation
+* Style working directory list
+* Update help function
+* Test/optimize on windows/linux
+
+## Contributing
+
+Send a pull request! Check out the list of todos
--- index.js
+++ index.js
@@ -5,22 +5,51 @@ var fs = require('fs');
 const { exec } = require('child_process');
 const { getInstalledPathSync } = require('get-installed-path')
 
+//Location of saved directory list
 var localDataStore = getInstalledPathSync('working-directory-manager') + "\\data.json";
 
+//Base program information
 program
-    .version('0.1.0')
+    .version('0.5.0', '-v, --version')
+    .usage('[command] <shortcut>')
+
+//Switch directory function
+program
     .arguments('<shortcut>')
+    .description('If no subcommand is given, the program opens the selected working directory, if it exists.')
     .action(function (shortcut) {
         fs.readFile(localDataStore, 'utf8', function(err, contents) {
-            //console.log(contents);
             try {
                 result = JSON.parse(contents);
-                try {
+                if(result[shortcut]) {
                     let shortcutPath = result[shortcut];
                     console.log("Going to " + shortcut);
-                    exec("start cmd.exe /K cd " + shortcutPath);
-                }
-                catch(e) {
+                    if(process.platform === 'win32'){
+                        exec("start cmd.exe /K cd " + shortcutPath, (error, stdout, stderr) => {
+                          if (error) {
+                            console.error(`exec error: ${error}`);
+                            return;
+                          }
+                        });
+                    } else if(process.platform === 'darwin'){
+                        exec("open -a Terminal " + shortcutPath, (error, stdout, stderr) => {
+                          if (error) {
+                            console.error(`exec error: ${error}`);
+                            return;
+                          }
+                        });  
+                    } else if(process.platform === 'linux'){
+                        exec("xterm -e \"cd " + shortcutPath + "\"", (error, stdout, stderr) => {
+                          if (error) {
+                            console.error(`exec error: ${error}`);
+                            return;
+                          }
+                        });  
+                    } else {
+                        console.log(`OS not supported. Look for future updates`);
+                    }
+                    
+                } else {
                     console.error("Shortcut not found.")
                     process.exit(1);
                 }
@@ -32,39 +61,43 @@ program
         });
     });
 
+//Save directory function
 program
-    .command('save <name> [loc]').alias('s')
-    .action(function (name, loc) {
+    .command('save <shortcut> [loc]').alias('s')
+    .description('\nSaves the current directory under the alias <shortcut>. If [loc] is given, it is saved instead.\n')
+    .action(function (shortcut, loc) {
         var shortcutPath;
         if(loc){
             shortcutPath = loc;
         }else{
             shortcutPath = process.cwd();
         }
-        console.log("Saving " + shortcutPath + " as " + name);
+        console.log("Saving " + shortcutPath + " as " + shortcut);
         fs.readFile(localDataStore, 'utf8', function(err, contents) {
             try {
                 result = JSON.parse(contents);
-                result[name] = shortcutPath;
+                result[shortcut] = shortcutPath;
                 fs.writeFile(localDataStore, JSON.stringify(result), 'utf8');
             }
             catch(e) {
-                fs.writeFile(localDataStore, JSON.stringify({[name]: shortcutPath}), 'utf8');
+                fs.writeFile(localDataStore, JSON.stringify({[shortcut]: shortcutPath}), 'utf8');
             }
         });
         
     });
 
+//List saved directories function
 program
-    .command('list [name]').alias('l')
-    .action(function (name) {
+    .command('list [shortcut]').alias('l')
+    .description('\nList all saved directories. If [shortcut] is given, the program will search for that particular alias.\n')
+    .action(function (shortcut) {
         fs.readFile(localDataStore, 'utf8', function(err, contents) {
             try {
                 result = JSON.parse(contents);
-                if(name){
-                    if(result[name]){
-                        let shortcutPath = result[name];
-                        console.log(name + "\t- " + shortcutPath);
+                if(shortcut){
+                    if(result[shortcut]){
+                        let shortcutPath = result[shortcut];
+                        console.log(shortcut + "\t- " + shortcutPath);
                     } else {
                         console.error("Shortcut not found.")
                     }
@@ -81,45 +114,37 @@ program
             }
             catch(e) {
                 console.error("No shortcuts saved.");
+                process.exit(1);
             }
         });
     });
 
+//Remove directory function
 program
-    .command('remove <name>').alias('r')
-    .action(function (name) {
+    .command('remove <shortcut>').alias('r')
+    .description('\nRemove the selected directory from the list of saved aliases, if it exists.\n')
+    .action(function (shortcut) {
         fs.readFile(localDataStore, 'utf8', function(err, contents) {
             try {
                 result = JSON.parse(contents);
-                if(result[name]) {
-                    delete result[name];
+                if(result[shortcut]) {
+                    delete result[shortcut];
                     var returnJSON = JSON.stringify(result);
                     fs.writeFile(localDataStore, returnJSON, 'utf8');
-                    console.log("Removed " + name + " from saved workspaces.");
+                    console.log("Removed " + shortcut + " from saved workspaces.");
                 } else {
                     console.error("Shortcut not found.");
+                    process.exit(1);
                 }
             }
             catch(e) {
                 console.error("No shortcuts saved.");
+                process.exit(1);
             }
         });
     });
 
-program
-    .command('root')
-    .action(function (name, loc) {
-        console.log(localDataStore);        
-    });
-
 program.parse(process.argv);
 
+//Print help if user types no arguments
 if (!program.args.length) program.help();
-
-// console.log(packageLocation);
-// console.log(`Current directory: ${process.cwd()}`);
-// shell.exec('echo Current directory: ${process.cwd()}');
-
-//functions to make:
-//is Valid file path
-//
